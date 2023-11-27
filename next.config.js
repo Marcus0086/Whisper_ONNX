f6c8b84c1daebb07cddd879b8b4108bc8fd97c7b
Marcus0086 <guptamarcus42@gmail.com>
2023-11-28 00:16:25 +0530

    Removed model from repo

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:41ee9049e71c3fbfe2f1481539663b6d0a4d9cb343a753513da80d642adba8fc
-size 882
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
+        config.plugins.push(
+            new copyWebpackPlugin({
+                patterns: [
+                    {
+                        from: './node_modules/onnxruntime-web/dist/ort-wasm.wasm',
+                        to: 'static/chunks',
+                    },
+                    {
+                        from: './node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
+                        to: 'static/chunks',
+                    }
+                ],
+            })
+        )
+        return config;
+    }
+}
+
+module.exports = nextConfig
