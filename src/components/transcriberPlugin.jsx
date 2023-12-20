9cbbaf89c0c11bada20446f9b1c6d631df90e97b
Marcus0086 <guptamarcus42@gmail.com>
2023-12-20 17:33:59 +0530

    new

--- src/components/transcriberPlugin.jsx
+++ src/components/transcriberPlugin.jsx
@@ -6,15 +6,13 @@ class Transcriber {
 
     audioRecorder = audioRecorder
     audioProcessor = audioProcessor;
-    whiper = new Whisper();
+    whisper = new Whisper();
     isRecording = false;
     data = {};
 
-    constructor({ data, config, api }) {
+    constructor({ data }) {
         this.data = data;
-        if (Object.keys(data).length === 0) {
-            this.whiper.loadModel('/_next/static/chunks/model/whisper.onnx')
-        }
+        this.whisper.loadModel('/_next/static/chunks/model/whisper.onnx')
     }
     static get toolbox() {
         return {
@@ -24,44 +22,45 @@ class Transcriber {
     }
 
     render() {
-        const button = document.createElement('div');
-        button.className = 'ce-paragraph cdx-block cursor-pointer';
-        if (Object.keys(this.data).length === 0) {
-            button.innerText = 'Click to Record';
-            button.onclick = async () => {
-                if (!this.isRecording) {
-                    this.audioRecorder?.start().then(() => {
-                        button.innerText = 'Recording...';
-                        this.isRecording = true;
-                    });
-                } else {
-                    this.audioRecorder?.stop().then(blob => {
-                        if (blob) {
-                            button.innerText = "Processing...";
-                            this.audioProcessor?.transcribe(blob, this.whiper).then(text => {
-                                button.innerText = text;
-                                this.isRecording = false;
-                                button.contentEditable = true;
-                                button.classList.remove('cursor-pointer')
-                                button.onclick = () => { };
-                            })
-                        }
-                    })
-                }
+        const parent = document.createElement('div');
+        parent.className = 'flex w-full items-center justify-center';
+        const div = document.createElement('div');
+        const button = document.createElement('button');
+        div.contentEditable = true;
+        div.innerText += this.data.text || '';
+        div.className = 'ce-paragraph cdx-block w-full';
+        button.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" class="h-8 w-8" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M128,176a48.05,48.05,0,0,0,48-48V64a48,48,0,0,0-96,0v64A48.05,48.05,0,0,0,128,176ZM96,64a32,32,0,0,1,64,0v64a32,32,0,0,1-64,0Zm40,143.6V232a8,8,0,0,1-16,0V207.6A80.11,80.11,0,0,1,48,128a8,8,0,0,1,16,0,64,64,0,0,0,128,0,8,8,0,0,1,16,0A80.11,80.11,0,0,1,136,207.6Z"></path></svg>';
+        button.onclick = async () => {
+            if (!this.isRecording) {
+                this.audioRecorder?.start().then(() => {
+                    button.innerHTML = 'Recording...';
+                    this.isRecording = true;
+                });
+            } else {
+                this.audioRecorder?.stop().then(blob => {
+                    if (blob) {
+                        button.innerHTML = "Processing...";
+                        button.disabled = true;
+                        button.classList.add('cursor-not-allowed');
+                        this.audioProcessor?.transcribe(blob, this.whisper).then(text => {
+                            div.innerText += text;
+                            this.isRecording = false;
+                            button.disabled = false;
+                            button.classList.remove('cursor-not-allowed');
+                            button.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" class="h-8 w-8" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M128,176a48.05,48.05,0,0,0,48-48V64a48,48,0,0,0-96,0v64A48.05,48.05,0,0,0,128,176ZM96,64a32,32,0,0,1,64,0v64a32,32,0,0,1-64,0Zm40,143.6V232a8,8,0,0,1-16,0V207.6A80.11,80.11,0,0,1,48,128a8,8,0,0,1,16,0,64,64,0,0,0,128,0,8,8,0,0,1,16,0A80.11,80.11,0,0,1,136,207.6Z"></path></svg>';
+                        })
+                    }
+                })
             }
-        } else {
-            button.innerText = this.data.text;
-            button.contentEditable = true;
-            button.classList.remove('cursor-pointer')
-            button.onclick = () => { };
         }
-        return button;
+        parent.appendChild(div);
+        parent.appendChild(button);
+        return parent;
     }
 
     save(blockContent) {
-        console.log(blockContent);
         return {
-            text: blockContent.innerText
+            text: blockContent.children[0].innerText
         }
     }
 }
